ae732890a0b847a4d882eeca91d4078c02b27d3b
Unmesh28 <unmesh.dabhade@gmail.com>
2023-01-03 14:30:21 +0530

    Loggig added

--- Thread.py
+++ Thread.py
@@ -19,7 +19,7 @@ from PyQt5.QtWidgets import QApplication
 import logging
 #logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-class WorkerThread (QObject, multiprocessing.Process):
+class WorkerThread (QObject, QThread):
     
     sig = pyqtSignal(int, int)
     logging.debug('Test Logging')
