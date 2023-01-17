daa4d2aa60a5c295d5c3dcdc7c291f2ed694b004
Unmesh28 <unmesh.dabhade@gmail.com>
2023-01-17 15:29:52 +0530

    Update

--- mainWindow.py
+++ mainWindow.py
@@ -717,7 +717,7 @@ class Ui_MainWindow(object):
 
     def ThreadOpen(self):
         print('Inside Thred')
-        self.worker = NewWorkerThread(self)
+        self.worker = WorkerThread(self)
         self.worker.start()
         self.worker.sig.connect(self.changeImg)
         #self.worker.moveToThread(QThread())
